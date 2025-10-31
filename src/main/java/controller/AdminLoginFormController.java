d2ba0652fcd4d3f68586918f85b82370f06d8950
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2025-10-31 16:15:18 +0530

    initial commit

---
+++ src/main/java/controller/AdminLoginFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
+public class AdminLoginFormController {
+
+    @FXML
+    private Button btnAdminLogIn;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnAdminLogInOnAction(ActionEvent event) {
+
+    }
+
+}
