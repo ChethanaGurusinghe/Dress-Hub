49c761d16cdf59e81fe64565ceca295ef82b29b8
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2025-11-02 11:46:41 +0530

    EmployeeManagement UI done

---
+++ src/main/java/controller/EmployeeManagementController.java
@@ -0,0 +1,110 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EmployeeManagementController {
+
+    @FXML
+    private Button btnAddProduct;
+
+    @FXML
+    private Button btnAdminDashboard;
+
+    @FXML
+    private Button btnAdminManagement;
+
+    @FXML
+    private Button btnCategoryManagement;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnEmployeeManagement;
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private Button btnProductManagement;
+
+    @FXML
+    private Button btnSupplierManagement;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colEmployeeId;
+
+    @FXML
+    private TableColumn<?, ?> colEmployeeName;
+
+    @FXML
+    private TableColumn<?, ?> colPhoneNo;
+
+    @FXML
+    private TextField searchTxtFeild;
+
+    @FXML
+    private TableView<?> tblEmployee;
+
+    @FXML
+    void btnAddProductOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAdminDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAdminManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCategoryManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProdutManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManagementOnAction(ActionEvent event) {
+
+    }
+
+}
