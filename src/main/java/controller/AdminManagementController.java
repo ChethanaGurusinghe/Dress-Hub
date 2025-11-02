939ba0d10c64572e38245ae0c0b0b0a336458447
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2025-11-02 20:46:11 +0530

    AdminManagement UIs done

---
+++ src/main/java/controller/AdminManagementController.java
@@ -0,0 +1,110 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AdminManagementController {
+
+    @FXML
+    private Button btnAddAdmin;
+
+    @FXML
+    private Button btnAdminDashboard;
+
+    @FXML
+    private Button btnAdminManagement;
+
+    @FXML
+    private Button btnCategoryManagement;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private Button btnEmployeeManagement;
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private Button btnProductManagement;
+
+    @FXML
+    private Button btnSupplierManagement;
+
+    @FXML
+    private TableColumn<?, ?> colAdminId;
+
+    @FXML
+    private TableColumn<?, ?> colAdminName;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colPhoneNo;
+
+    @FXML
+    private TextField searchTxtFeild;
+
+    @FXML
+    private TableView<?> tblAdmin;
+
+    @FXML
+    void btnAddAdminOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAdminDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAdminManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCategoryManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProdutManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManagementOnAction(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/controller/SupplierManagementController.java
+++ src/main/java/controller/SupplierManagementController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
 
 public class SupplierManagementController {
 
+    public TableView tblSupplier;
     @FXML
     private Button btnAddSupplier;
 
