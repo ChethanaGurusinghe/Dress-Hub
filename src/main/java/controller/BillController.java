001754aaa2182d1d36ee4e85ea4764ea56975b89
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2025-11-04 12:19:05 +0530

    InvoicePrint UIs done

---
+++ src/main/java/controller/BillController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class BillController {
+
+    @FXML
+    private Label lblBillNo;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblTotalAmount;
+
+}
