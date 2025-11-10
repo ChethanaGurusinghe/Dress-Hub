69a6157f7e2e6fc8c5ccffb33e298cdbf1931b3e
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2025-11-10 18:57:53 +0530

    SupplierDto done

---
+++ src/main/java/model/dto/Supplier.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Supplier {
+
+    private String supCode;
+    private String compName;
+    private String supName;
+    private String phone;
+    private String email;
+    private String address;
+    private String notes;
+}
